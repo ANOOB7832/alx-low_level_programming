4c62d4f586169c83dcb22fd641f942f5d04c3784
A NOOB <anoob@A_NOOB_PC.localdomain>
2022-06-02 23:57:41 +0200

    Size of various filetypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+/**
+ * main - Entry point
+ * printf - print on screen
+ * sizeof - finds sizes of data types
+ */
+
+int main(void)
+{
+printf("Size of a char: %2d byte(s) \n", sizeof(char));
+printf("Size of an int: %2d byte(s) \n", sizeof(int));
+printf("Size of a long int: %2d byte(s) \n", sizeof(long int));
+print("Size of a long long int: %2d byte(s) \n", sizeof(long long int));
+printf("Size of a float: %2d bytes(s) \n", sizeof(float));
+return (0);
+}
