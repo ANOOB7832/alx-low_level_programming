555ea486a30774488c801de31ed22999343f7a07
A NOOB <anoob@A_NOOB_PC.localdomain>
2022-06-08 22:42:34 +0200

    addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,9 +2,11 @@
 /**
  * add - adds two integers and returns the result
  * Return: Always 0 (Success)
+ * @a: integer a
+ * @b: integer b
  *
  */
 int add(int a, int b)
 {
-	return (a + b)
+	return (a + b);
 }
