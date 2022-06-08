e7b0116f3f6e4c3c994c291338278077fe04e768
A NOOB <anoob@A_NOOB_PC.localdomain>
2022-06-08 21:47:31 +0200

    isalphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - Checks if character is alphabetic
+ * @c: character to check
+ * Return: 1 (Is alphabetic)
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
