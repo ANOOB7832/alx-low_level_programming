d32f38943111d28d04ef563dcbf7842cc4564c8e
A NOOB <anoob@A_NOOB_PC.localdomain>
2022-06-10 23:32:33 +0200

    project files mass commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal line of length n
+ * @n: length of diagonal line
+ * Return: void
+ */
+void print_diagonal(int n);
+{
+	int a, b;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		for (a = 0; b < a; b++)
+		{
+			_putchar(' ');
+		}
+	_putchar('\\');
+	_putchar('\n');
+	}
+}
+
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - prints a sqaure of size size
+ * @size: the size of the square
+ * Return: void
+ */
+void print_square(int size)
+{
+	if (size < 0)
+	{
+		_putchar('\n');
+		for (a = 0; b < (size); b++)
+		{
+			_putchar('#')
+		}
+		_putchar('\n');
+	}
+}
