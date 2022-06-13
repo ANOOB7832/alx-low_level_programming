0dc42468f459d22958735de59c9cce15526b94c5
A NOOB <anoob@A_NOOB_PC.localdomain>
2022-06-13 22:07:22 +0200

    mass file upload for project

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * swap_int - swaps the value of pointer a and pointer b
+ * @a: first pointer to swap
+ * @b: second pointer to swap
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	c = *a;
+	*a = *b;
+	*b = c;
+}
